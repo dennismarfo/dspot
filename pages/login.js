fb32d9d8311814331dc60df0e7523790bed234aa
dennismarfo <dennismarfo@gmail.com>
2022-02-13 14:08:47 -0500

    custom login page

--- pages/login.js
+++ pages/login.js
@@ -2,12 +2,12 @@ import {getProviders, signIn} from "next-auth/react";
 
 function Login({ providers }) {
     return (
-        <div>
+        <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <img className="w-52 mb-5" src="http://links.papareact.com/9xl" alt=""/>
 
             {Object.values(providers).map((provider) => (
                 <div>
-                    <button>Login with {provider.name}</button>
+                    <button className="bg-[#18D860] text-white p-5 rounded-full" onClick={() => signIn(provider.id, {callbackUrl: "/"})}>Login with {provider.name}</button>
                 </div>
             ))}
         </div>
