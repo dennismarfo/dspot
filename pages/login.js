c5a35a0b9d41ffdbeaae8e25dba3fb53ef28e1f5
dennismarfo <dennismarfo@gmail.com>
2022-02-13 12:17:20 -0500

    correction on nextAuth moving folder

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,28 @@
-function Login() {
+import {getProviders, signIn} from "next-auth/react";
+
+function Login({ providers }) {
     return (
         <div>
+            <img className="w-52 mb-5" src="http://links.papareact.com/9xl" alt=""/>
 
+            {Object.values(providers).map((provider) => (
+                <div>
+                    <button>Login with {provider.name}</button>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 
 export default Login;
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers,
+        }
+    }
+}
